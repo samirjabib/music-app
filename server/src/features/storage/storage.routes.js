4567612e292c63db1cc28975f75312e8110e5a70
samirjabib <samirdev9669@gmail.com>
2023-01-19 13:29:59 -0500

    add multer

--- server/src/features/storage/storage.routes.js
+++ server/src/features/storage/storage.routes.js
@@ -3,6 +3,30 @@ const express = require("express")
 const { postItems} = require('./storage.controller')
 const storageRouter = express.Router()
 
+const multer = require("multer")
+
+/*
+ */
+
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        const pathStorage = `${__dirname}/../../storage` //Le indicamos la ruta donde guardaremos los archivos
+
+        cb(null, pathStorage)
+    },
+    filename:function(req, file, cb){
+        const ext = file.originalname.split(".").pop() //Separamos el array por el . y agarramos el valor final
+        const filename = `file${Date.now()}.${ext}}` //Generamos un nombre random
+        cb(null, filename)
+    },
+})
+
+
+
+
+/*
+ */
+
 storageRouter.post("/",postItems )
 
 
