2d94c4def68b6536e0f03196ebf21c62976425cc
samirjabib <samirdev9669@gmail.com>
2023-01-23 15:21:55 -0500

    add

--- server/src/middlewares/authLogin.js
+++ server/src/middlewares/authLogin.js
@@ -10,17 +10,19 @@ const authMiddleware = async(req, res, next) => {
 
         const token = req.headers.authorization.split(' ').pop();
         const dataToken = await verifyToken(token);
+        console.log(dataToken)
 
         if(!dataToken._id){
             return next(new AppError("ERROR_ID_TOKEN", 401));
         }
 
-        const user = await userModel.findById(_id)
+        const user = await userModel.findById(dataToken._id)
         req.user = user
 
         next()
     } catch (error) {
-        
+        console.log(error)
+        next(error)
     }
 }
 
